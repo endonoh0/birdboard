018d1aa3fbce531e082396be3e1cc633971aeaca
Eric Ho <endonoho@gmail.com>
2020-05-02 23:00:15 -0700

    Episode 9

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="flex items-center mb-3">
         <a href="/projects/create">New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}"
-                    >
-                        {{ $project->title }}
-                </a>
-            </li>
+            <div class="bg-white mr-4 p-4 rounded shadow-sm w-1/3" style="height: 200px">
+                <h3 class="font-normal font-bold text-xl py-4">{{ $project->title }}</h3>
+
+                <div class="text-gray-500">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
+            </div>
         @empty
-            <li>No projects yet.</li>
+            No projects yet...
         @endforelse
-    </ul>
+    </div>
 @endsection
