46cfc68a1ea4f02c033018fcc3fbf0822767c02e
Eric Ho <endonoho@gmail.com>
2020-05-07 16:33:55 -0700

    Episode 32

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mystyle.css') }}" rel="stylesheet">.
 </head>
 <body class="bg-gray-100 ">
     <div id="app">
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -7,7 +7,21 @@
                 <a href="/projects" class="text-sm text-gray-500 font-normal">My Projects</a> / {{ $project->title }}
             </p>
 
-            <a href="{{ $project->path() . '/edit' }}" class="button">Edit Project</a>
+            <div class="flex items-center">
+                @foreach ($project->members as $member)
+                    <img
+                        src="{{ gravatar_url($member->email) }}"
+                        alt="{{ $member->name }}'s avatar"
+                        class="rounded-full w-8 mr-2">
+                @endforeach
+
+                <img
+                    src="{{ gravatar_url($project->owner->email) }}"
+                    alt="{{ $project->owner->name }}'s avatar"
+                    class="rounded-full w-8 mr-2">
+
+                <a href="{{ $project->path() . '/edit' }}" class="button ml-4">Edit Project</a>
+            </div>
         </div>
     </header>
 
